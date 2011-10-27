747729c8dd00e8529873037ab99247e2b1905c73
Samkio <supercool_monkey@hotmail.co.uk>
2011-10-27 13:42:24 +0100

    Changed Zinc Ingot to Runite Ingot.

---
+++ src/me/samkio/mo/items/ingots/RuniteIngot.java
@@ -0,0 +1,12 @@
+package me.samkio.mo.items.ingots;
+
+import me.samkio.mo.MoarStuph;
+
+public class RuniteIngot extends BaseIngot{
+
+	public RuniteIngot(MoarStuph plugin) {
+		super(plugin, "Runite Ingot", "Zinc");
+		//TODO give Runite image
+	}
+
+}
