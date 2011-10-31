36d2ca72bb59e3ebfdcb25ff1a73850cc860a8ba
Chris Dusovic <cdusovic2411@gmail.com>
2011-10-30 23:13:32 -0400

    Added a central onItemInteract() for foods.

--- src/me/samkio/mo/items/food/BaseFood.java
+++ src/me/samkio/mo/items/food/BaseFood.java
@@ -2,6 +2,8 @@ package me.samkio.mo.items.food;
 
 import me.samkio.mo.MoarStuph;
 
+import org.bukkit.block.BlockFace;
+import org.getspout.spoutapi.block.SpoutBlock;
 import org.getspout.spoutapi.material.item.GenericCustomItem;
 import org.getspout.spoutapi.player.SpoutPlayer;
 
@@ -17,5 +19,37 @@ public class BaseFood extends GenericCustomItem {
 	
 	public void consume(SpoutPlayer player) {
 		player.getItemInHand().setAmount(player.getItemInHand().getAmount()-1);
+	}
+	
+	@Override
+	public boolean onItemInteract(SpoutPlayer player, SpoutBlock block,	BlockFace face) {
+		
+		if (player.getFoodLevel() != 20 || player.getHealth() != 20) {
+			
+			if (player.getFoodLevel() != 20 && foodAmount != 0) {
+				
+				player.setFoodLevel(player.getFoodLevel() + foodAmount);
+				
+				if (player.getFoodLevel() > 20) {
+					
+					player.setFoodLevel(20);
+				}
+			}
+			else if (player.getHealth() != 20 && healingAmount != 0) {
+				
+				player.setHealth(player.getHealth() + healingAmount);
+				
+				if (player.getHealth() > 20) {
+					
+					player.setHealth(20);
+				}
+			}
+			consume(player);
+			return true;
+		}
+		
+		else {
+			return false;
+		}
 	}
 }
